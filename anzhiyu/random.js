c1083ca93f2449d5598ebd4ada3fd6bf5aadf48c
zzy-swimming <zzy20060811@gmail.com>
2025-07-29 03:17:21 +0800

    Site updated: 2025-07-29 03:17:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/28/hello-world/","2025/07/28/这是一篇新的博文/"];function toRandomPost(){
+var posts=["2025/07/28/这是一篇新的博文/","2025/07/28/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
